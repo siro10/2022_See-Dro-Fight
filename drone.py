066be67290883dc7dd65cdcac26602ea4ecf8001
siro10 <siro.white.blanc@gmail.com>
2022-10-08 09:42:48 +0900

    moveSize

--- drone.py
+++ drone.py
@@ -11,18 +11,17 @@ d=時計回り
 t=離陸
 l=着陸
 esc=テロ制御の終了
-
 """
 
+#a
+change_kando=1#変えるやつ
 
 
 cap=cv2.VideoCapture(1,cv2.CAP_DSHOW)
 
 class FlightDroneClass():
-    #接続開始
     def __init__(self):
-        #ドローンとの接続
-        self.movesize=70#テロの動きの大きさを決める"後でself化
+        #self.movesize = 70
         
         self.players = []
 
@@ -32,9 +31,25 @@ class FlightDroneClass():
         self.qrDetector = cv2.QRCodeDetector()
         #ドローン準備
         self.tello = Tello()
+        #メインループ
+        self.whileCheck = True
 
     #接続開始
     def start(self):
+        #a
+        if change_kando==1:
+            self.kando1=20
+            self.kando2=30
+            self.kando3=40
+        elif change_kando==2:
+            self.kando1=30
+            self.kando2=50
+            self.kando3=70
+        elif change_kando==3:
+            self.kando1=40
+            self.kando2=70
+            self.kando3=100
+
         #ドローンとの接続
         self.tello.connect()
 
@@ -53,29 +68,25 @@ class FlightDroneClass():
     def getImage(self):
         #ドローンからの画像の取得
         self.image = self.tello.get_frame_read().frame
-        self.writeimage = None
-        self.writeimage=self.image
         #QRコード読み取り
         data,bbox,rectifiedImage = self.qrDetector.detectAndDecode(self.image)
 
-        #文字列を取得したら内容を表示
+        #点数の計算、タイマーリセット
         if data:
-            print(data)
-
-            #プレイヤーの点数計算(複数プレイヤー対応)
-            if time.time()-self.timePoint>=5 :
-                print("players:")
-                print(self.players)
+            print(data)#qrコードの内容
                 hight = 1
                 for i in self.players:
                     #点数追加、タイマーリセット
                     if data == i[0]:
-                        self.players[hight - 1][1] = self.players[hight -  1][1] + 1
-                        self.timePoint=time.time()
+                    if time.time()-i[3]>=5 :
+                        self.players[hight - 1][1] = self.players[hight - 1][1] + 1
+                        self.players[hight - 1][3]=time.time()
                         return
                     hight = hight + 1
                 #新規登録
-                self.players.append([data,0,(50*hight)])
+            self.players.append([data,1,(50*hight),time.time()])
+
+                
 
 
     #ディスプレイに表示
@@ -83,11 +94,11 @@ class FlightDroneClass():
             print("players:")
             print(self.players)
             #点数表示の準備
-            if self.players:
-                for i in self.players:
-                    cv2.putText(self.writeimage, (i[0] + ":" +str(i[1])), (0, i[2]), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 5, cv2.LINE_AA)
+            cv2.putText(self.image, (str(self.gameTime  - int(time.time()-self.timeStart)) + "seconds left"), (0, 50), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 5, cv2.LINE_AA)
+            for i in self.players:
+                cv2.putText(self.image, (i[0] + ":" +str(i[1])), (0, 50 + i[2]), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 5, cv2.LINE_AA)
             #表示
-            cv2.imshow('SEE-DRO FIGHT!', self.writeimage)
+            cv2.imshow('SEE-DRO FIGHT!', self.image)
             
     #キーボード操作
     def droneControl(self,key):#left,rigth   back,foward   up,down   cw,ccw
@@ -148,8 +159,7 @@ class FlightDroneClass():
             print("esc has pressed")
             self.displayTotalScore()
             self.tello.land()
-            cv2.destroyAllWindows()
-            return
+            
 
     
     #時間制限で終了
@@ -158,16 +168,20 @@ class FlightDroneClass():
         if time.time()-self.timeStart>=self.gameTime :
             self.displayTotalScore()
             self.tello.land()
-            cv2.destroyAllWindows()
-            return
 
     #リザルト表示
     def displayTotalScore(self) :
+        self.image = self.tello.get_frame_read().frame
+        #合計点数
+        points = 0
         for i in self.players:
-            points = 0
             points = points + i[1]
-        print(str(points) + "bbbbbbbbbbbbbbbbbb")
-        cv2.putText(self.image, ("Total score:" +str(points)), (0, 50), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 5, cv2.LINE_AA)
+        #合計スコア表示
+        cv2.putText(self.image, ("Total score:" + str(points)), (0, 50), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 5, cv2.LINE_AA)
+        #各プレイヤーの点数表示
+        for i in self.players:
+                cv2.putText(self.image, (i[0] + ":" +str(i[1])), (0, 50 + i[2]), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 5, cv2.LINE_AA)
+        
         cv2.imshow('SEE-DRO FIGHT!', self.image)
             
 
@@ -177,12 +191,14 @@ class FlightDroneClass():
         self.timePoint = time.time()#点数
         self.timeStart = time.time()#時間制限
 
-        while 1:
+        while self.whileCheck:
             self.getImage()#画像の取得、QRコードの読み取り
             self.displayScreen()#ドローンからの画像の表示
-            self.quitScreen()#時間制限
 
             #キー入力の受けとり、ドローンの操作
             key = cv2.waitKey(30)
-
             self.droneControl(key)
+
+            self.quitScreen()#時間制限
+        cv2.destroyAllWindows()
+        return
